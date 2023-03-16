518cc55f959043b3454bc736b78189ebebf6f157
tayyab javed <tayyabdev@outlook.com>
2023-03-16 10:44:34 +0000

    env ify

--- frontend/src/assets/fb.js
+++ frontend/src/assets/fb.js
@@ -4,16 +4,17 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKCihPOJ5er9fsl85BOtNQabKYi9y_9cE",
-  authDomain: "streamcatcherui.firebaseapp.com",
-  projectId: "streamcatcherui",
-  storageBucket: "streamcatcherui.appspot.com",
-  messagingSenderId: "716651623898",
-  appId: "1:716651623898:web:a34fd39d690c6c92877ce3",
-  measurementId: "G-8B1EBXZ46P"
+  apiKey: import.meta.env.VITE_FB_apiKey,
+  authDomain: import.meta.env.VITE_FB_authDomain,
+  projectId: import.meta.env.VITE_FB_projectId,
+  storageBucket: import.meta.env.VITE_FB_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FB_messagingSenderId,
+  appId: import.meta.env.VITE_FB_appId,
+  measurementId: import.meta.env.VITE_FB_measurementId
 };
 
 // Initialize Firebase
--- frontend/src/pages/dashboard/home.jsx
+++ frontend/src/pages/dashboard/home.jsx
@@ -15,7 +15,7 @@ function DashboardHome() {
 
 
   useEffect(()=>{
-    setEmail(user)
+    setEmail(user[0])
   },[user])
 
 
