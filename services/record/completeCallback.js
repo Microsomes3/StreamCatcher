429c9b43fd49d500f6db0f25236e542257098e3c
tayyab javed <tayyabdev@outlook.com>
2023-03-19 00:02:08 +0000

    done

---
+++ services/record/completeCallback.js
@@ -0,0 +1,16 @@
+const aws = require('aws-sdk');
+
+
+module.exports.handler = async (event) => {
+
+    const body = json.parse(event.body);
+
+    console.log(body);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello from Lambda',
+        }),
+    }
+};
