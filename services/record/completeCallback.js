cd92a3bcd349a9d0eee4b91736ee32c6608534a1
Tayyab Javed <tayyab.javed@grv.media>
2023-03-19 11:01:05 +0000

    callback

--- services/record/completeCallback.js
+++ services/record/completeCallback.js
@@ -3,14 +3,26 @@ const aws = require('aws-sdk');
 
 module.exports.handler = async (event) => {
 
-    const body = JSON.parse(event.body);
+    const {
+        requestId,
+        key 
+    } = JSON.parse(event.body);
 
-    console.log(body);
+    if (!requestId || !key) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'Missing required parameters',
+            }),
+        };
+    }
+
 
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello from Lambda',
+           requestId,
+           key
         }),
     }
 };
